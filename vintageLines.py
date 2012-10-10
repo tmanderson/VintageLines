a2cc13c3a164d9f533af072bf56f6dc0670a436c
Fredrik Ehnbom <quarnster@gmail.com>
2012-10-10 13:33:45 +0200

    Only update when needed rather than polling all the time. Accomplished via on_selection_modified + add_on_change for the view's settings. This way we know if the current line changes or if the current vintage mode changes.

--- vintageLines.py
+++ vintageLines.py
@@ -1,59 +1,80 @@
 import os, shutil, math, sublime, sublime_plugin
 
-class VintageLinesCommand(sublime_plugin.TextCommand):
-	def run(self, args, show):
-
-		if show == True:
-			self.removeRelativeNumbers()
-			self.showRelativeNumbers()
-		else:
-			self.removeRelativeNumbers()
+class VintageLinesEventListener(sublime_plugin.EventListener):
+	def __init__(self):
+		self.in_check_settings = False
+		self.icon_count = 99
 
 	def showRelativeNumbers(self):
 		view = self.view
 
 		view.settings().set('line_numbers', False)
 
-		cur_line = view.rowcol(view.sel()[0].begin())[0] - view.rowcol(view.visible_region().begin())[0]
+		cur_line = view.rowcol(view.sel()[0].begin())[0]
+		start_line = max(cur_line-self.icon_count, 0)
+		end_line = min(cur_line+self.icon_count+1, self.view.rowcol(self.view.size())[0])
 
-		lines = self.view.lines(view.visible_region())
-		icon_count = 99
+		lines = self.view.lines(sublime.Region(self.view.text_point(start_line, 0), self.view.text_point(end_line, 0)))
 
-		for i in range(max(cur_line-icon_count, 0), min(cur_line+icon_count+1, len(lines))):
+		for i in range(start_line, end_line):
 			name = 'linenum' + str(i)
 			icon = str(int(math.fabs(cur_line - i)))
 
-			view.add_regions(name, [lines[i]], 'linenums', "../VintageLines/icons/%s/%s" % (sublime.platform(), icon), sublime.HIDDEN)
+			view.add_regions(name, [lines[i-start_line]], 'linenums', "../VintageLines/icons/%s/%s" % (sublime.platform(), icon), sublime.HIDDEN)
 
 	def removeRelativeNumbers(self):
 		self.view.settings().set('line_numbers', True)
 
+		cur_line = self.view.settings().get('vintage_lines.line', 0)
+		max_lines = self.view.rowcol(self.view.size())[0]
+
 		# Remove all relative line number regions within viewport
-		for i in range(len(self.view.visible_region())):
+		for i in range(max(cur_line-self.icon_count, 0), min(cur_line+self.icon_count+1, max_lines)):
 			if self.view.get_regions('linenum' + str(i)):
 				self.view.erase_regions('linenum' + str(i))
 
-class VintageLinesListener(sublime_plugin.ApplicationCommand):
-	def run(self):
+	def checkSettings(self):
+		if self.in_check_settings:
+			# As this function is called when a setting changes, and its children also
+			# changes settings, we don't want it to end up in an infinite loop.
+			return
+		self.in_check_settings = True
+		if self.view:
+			settings = self.view.settings()
+
+			show = settings.get('command_mode') and not self.view.has_non_empty_selection_region()
+			mode = settings.get('vintage_lines.mode', False)
+			line = settings.get('vintage_lines.line', -1)
+			lines = settings.get('vintage_lines.lines', -1)
+
+			update = mode != show
+			update = update or line != self.view.rowcol(self.view.sel()[0].begin())[0]
+			update = update or lines != self.view.rowcol(self.view.size())[0]
+			if update:
+				if show:
+					self.removeRelativeNumbers()
+					self.showRelativeNumbers()
+				else:
+					self.removeRelativeNumbers()
+
+				self.view.settings().set('vintage_lines.line', self.view.rowcol(self.view.sel()[0].begin())[0])
+				self.view.settings().set('vintage_lines.mode', show)
+				self.view.settings().set('vintage_lines.lines', self.view.rowcol(self.view.size())[0])
+
+		self.in_check_settings = False
+
+	def on_activated(self, view):
+		self.view = view
+		if view:
+			view.settings().clear_on_change("VintageLines")
+			view.settings().set('vintage_lines.line', -1) # Just to force an update on activation
+			view.settings().add_on_change("VintageLines", self.checkSettings)
+
 		self.checkSettings()
 
-	def checkSettings(self):
-		window = sublime.active_window()
+	def on_selection_modified(self, view):
+		self.view = view
+		self.checkSettings()
 
-		if window:
-			view = window.active_view_in_group(window.active_group())
 
-			if view:
-				settings 	= view.settings()
 
-				if settings.get('command_mode'):
-					view.run_command('vintage_lines', {"show":True})
-				else:
-					view.run_command('vintage_lines', {"show":False})
-
-		sublime.set_timeout(self.checkSettings, 100)
-
-def run_vintage_lines():
-	sublime.run_command('vintage_lines_listener')
-
-sublime.set_timeout(run_vintage_lines, 500)
